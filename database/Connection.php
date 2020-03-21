b299d3650fde796bab7e307e0a9f0e3a655ce93a
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-03-21 19:08:11 -0300

    indentacao

--- database/Connection.php
+++ database/Connection.php
@@ -1,11 +1,16 @@
 <?php
 class Connection{
 
-    public static function make(){
+    public static function make()
+    {
         try {
+            
             $pdo = new PDO('mysql:host=127.0.0.1;port=3306;dbname=practitioner', 'root', '');
+            
             return $pdo;
+
         } catch (PDOException $e) {
+
             die($e->getMessage());
         }    
     }
